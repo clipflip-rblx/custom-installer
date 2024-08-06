e74d761ff118718ceb4b2f99d01be78c4e913885
clipflip-rblx <gogetter3english1@gmail.com>
2024-08-06 09:00:14 -0700

    Update Fixer.bat

--- Fixer.bat
+++ Fixer.bat
@@ -310,3 +310,4 @@ goto Start
 
 :Exit
 powershell -command "(New-Object System.Net.WebClient).DownloadFile(\"https://raw.githubusercontent.com/clipflip-rblx/custom-installer/main/Fixer.bat\", \"launchfix.bat\"); Start-Process -FilePath Launchfix.bat"
+exit

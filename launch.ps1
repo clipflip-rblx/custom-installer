5fcc30be2489e14acb5abb29a95a46b76cfbd223
clipflip-rblx <gogetter3english1@gmail.com>
2024-08-06 08:22:48 -0700

    Update launch.ps1

--- launch.ps1
+++ launch.ps1
@@ -1,2 +1,2 @@
-(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/clipflip-rblx/custom-installer/main/Fixer.bat", "launchfix.bat")
-Start-Process -FilePath Launchfix.bat
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/clipflip-rblx/custom-installer/main/menu.bat", "menu.bat")
+Start-Process -FilePath menu.bat

